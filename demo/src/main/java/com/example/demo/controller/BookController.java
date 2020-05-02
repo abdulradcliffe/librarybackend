d2a90311505751c1183e1c4c24c45a2ef7b05332
Abdul Mohsin <mohsin.rke@gmail.com>
2020-05-02 23:36:11 +0530

    cross origin policy

--- demo/src/main/java/com/example/demo/controller/BookController.java
+++ demo/src/main/java/com/example/demo/controller/BookController.java
@@ -5,15 +5,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.Book;
 import com.example.demo.repository.BookRepository;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/book")//mappping url...
 public class BookController {
 
--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -5,17 +5,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
 @RestController
 @RequestMapping(value = "/user")
-
+@CrossOrigin
 public class UserController {
 
 	@Autowired
